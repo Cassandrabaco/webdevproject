e592c498f642766a069075d0373d32ac2a12cb62
Cassandra Baco <qb202101466@wmsu.edu.ph>
2023-11-01 15:47:48 +0700

    Update add.php

--- functions/add.php
+++ functions/add.php
@@ -5,12 +5,13 @@
     </head>
     <body>
         <?php
+        //including the database connection file
           include_once("../dbConnection/mysqlconfig_connection.php");
 
           if(isset($_POST['Submit'])) {
             $code = $_POST['code'];
             $name = $_POST['name'];
-        
+            //checking empty fields
             if(empty($code) || empty($name)) {
                 if(empty($code)) {
                     echo "<font color='red'>Subject Code field is empty. </font><br/>";
@@ -18,10 +19,14 @@
                 if(empty($name)) {
                     echo "<font color='red'>Subject Name field is empty. </font><br/>";
                 }
+                //link to the previous page
                 echo "<br/><a href='javascript:self.history.back();'>Go Back</a>";
             }
             else {
+                //if all the fields are filled (not empty)
+                //insert data to database
                 $result = mysqli_query($dbc, "INSERT INTO tblsubject(subject_code, subject_name) VALUES('$code','$name')");
+                //display success message
                 echo "font color='green'>Data added successfully.";
                 echo "<br/><a href='../index.php'>View Result</a>";
             }

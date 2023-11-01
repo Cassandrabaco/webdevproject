060f3e6533c8188c2edcc8bcf8fd25297ce09335
Cassandra Baco <qb202101466@wmsu.edu.ph>
2023-11-01 16:18:48 +0700

    Update mysqlconfig_connection.php

--- dbConnection/mysqlconfig_connection.php
+++ dbConnection/mysqlconfig_connection.php
@@ -5,5 +5,5 @@
     DEFINE('DB_NAME', 'db');
 
     $dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
-    OR does('Could not connect to MySQL: '  . mysql_connect_error());
+    OR dies('Could not connect to MySQL: ' . mysqli_connect_error());
 ?>

6489408c8d7fd5fc7c568279794dd11da110d370
Cassandra <143484957+Cassandrabaco@users.noreply.github.com>
2024-11-16 22:36:59 +0800

    Delete dbConnection directory
    Signed-off-by: Cassandra <143484957+Cassandrabaco@users.noreply.github.com>

--- dbConnection/mysqlconfig_connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-    DEFINE('DB_USER', 'root');
-    DEFINE('DB_PASSWORD', 'root');
-    DEFINE('DB_HOST', 'localhost');
-    DEFINE('DB_NAME', 'db');
-
-    $dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
-    OR dies('Could not connect to MySQL: ' . mysqli_connect_error());
-?>
